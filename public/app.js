a8a5c672d0ba8966ef021a730fb3fb2e40cee8a7
Josh Eimer <jeimer3184@gmail.com>
2017-02-11 16:02:05 -0600

    Added auto-filing search bar

---
+++ public/app.js
@@ -0,0 +1,16 @@
+$(function(){
+	trackingData = [
+		{ id: 0, location: { zip: 63031, streetName: 'Bellarmine', block: 900	}, features: {	type: false, color: 'black', injured: false, size: 'medium', sex: false	}, otherNotes: 'Attempted to contact dog, but she ran as I got close',	picture: false },
+		{ id: 1, location: { zip: 63031, streetName: 'Bellarmine', block: 900	}, features: {	type: false, color: 'black', injured: false, size: 'medium', sex: false	}, otherNotes: 'Attempted to contact dog, but she ran as I got close',	picture: false },
+		{ id: 2, location: { zip: 63031, streetName: 'Bellarmine', block: 900	}, features: {	type: false, color: 'black', injured: false, size: 'medium', sex: false	}, otherNotes: 'Attempted to contact dog, but she ran as I got close',	picture: false },
+		{ id: 3, location: { zip: 63031, streetName: 'Bellarmine', block: 900	}, features: {	type: false, color: 'black', injured: false, size: 'medium', sex: false	}, otherNotes: 'Attempted to contact dog, but she ran as I got close',	picture: false },
+		{ id: 4, location: { zip: 63031, streetName: 'Bellarmine', block: 900	}, features: {	type: false, color: 'black', injured: false, size: 'medium', sex: false	}, otherNotes: 'Attempted to contact dog, but she ran as I got close',	picture: false },
+		{ id: 5, location: { zip: 63031, streetName: 'Bellarmine', block: 900	}, features: {	type: false, color: 'black', injured: false, size: 'medium', sex: false	}, otherNotes: 'Attempted to contact dog, but she ran as I got close',	picture: false },
+		{ id: 6, location: { zip: 63031, streetName: 'Bellarmine', block: 900	}, features: {	type: false, color: 'black', injured: false, size: 'medium', sex: false	}, otherNotes: 'Attempted to contact dog, but she ran as I got close',	picture: false },
+		{ id: 7, location: { zip: 63031, streetName: 'Bellarmine', block: 900	}, features: {	type: false, color: 'black', injured: false, size: 'medium', sex: false	}, otherNotes: 'Attempted to contact dog, but she ran as I got close',	picture: false },
+		{ id: 8, location: { zip: 63031, streetName: 'Bellarmine', block: 900	}, features: {	type: false, color: 'black', injured: false, size: 'medium', sex: false	}, otherNotes: 'Attempted to contact dog, but she ran as I got close',	picture: false },
+		{ id: 9, location: { zip: 63031, streetName: 'Bellarmine', block: 900	}, features: {	type: false, color: 'black', injured: false, size: 'medium', sex: false	}, otherNotes: 'Attempted to contact dog, but she ran as I got close',	picture: false },
+	]
+
+	console.log(trackingData[0])
+})
